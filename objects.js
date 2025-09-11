85a1def16b4101d2e2f160bd5d031439b98b8824
SR <saodat.riskiyeva@gmail.com>
2025-09-10 23:17:17 -0400

    creating objects with the help of blueprints

---
+++ objects.js
@@ -0,0 +1,16 @@
+class Job {
+  constructor(title, location, salary) {
+    this.title = title;
+    this.location = location;
+    this.salary = salary;
+  }
+}
+
+const developer = new Job("Developer", "New York", 120000);
+const designer = new Job("Designer", "San Francisco", 100000);
+const manager = new Job("Manager", "Chicago", 110000);
+const cook = new Job("Cook", "Los Angeles", 80000);
+console.log(developer);
+console.log(designer);
+console.log(manager);
+console.log(cook);
