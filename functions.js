01b71ac7a6634392c2a6f7474de1fcf397bdc9d9
SR <saodat.riskiyeva@gmail.com>
2025-09-10 22:45:17 -0400

    uncommenting functions

--- functions.js
+++ functions.js
@@ -1,17 +1,17 @@
-// function greetUser(name = "Guest") {
-//   console.log(`Hello, ${name}! Welcome to our website.`);
-// }
+function greetUser(name = "Guest") {
+  console.log(`Hello, ${name}! Welcome to our website.`);
+}
 
-// greetUser("Max"); // Default parameter
-// greetUser(); // Default parameter
+greetUser("Max"); // Default parameter
+greetUser(); // Default parameter
 
-// function sumUp(num1 = 0, num2 = 0, num3 = 0) {
-//   return num1 + num2 + num3;
-// }
+function sumUp(num1 = 0, num2 = 0, num3 = 0) {
+  return num1 + num2 + num3;
+}
 
-// console.log(sumUp(1, 2, 3)); // 6
-// console.log(sumUp(1, 2)); // NaN
-// console.log(sumUp(1)); // NaN
+console.log(sumUp(1, 2, 3)); // 6
+console.log(sumUp(1, 2)); // NaN
+console.log(sumUp(1)); // NaN
 
 function sumUp2(numbers) {
   let result = 0;
