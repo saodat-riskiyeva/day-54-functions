1a7241f44b7c9c6995de883fa9d56d4bf1b218b0
SR <saodat.riskiyeva@gmail.com>
2025-09-10 22:27:17 -0400

    a function with paramteres that can have default values

--- functions.js
+++ functions.js
@@ -2,4 +2,13 @@ function greetUser(name = "Guest") {
   console.log(`Hello, ${name}! Welcome to our website.`);
 }
 
+greetUser("Max"); // Default parameter
 greetUser(); // Default parameter
+
+function sumUp(num1 = 0, num2 = 0, num3 = 0) {
+  return num1 + num2 + num3;
+}
+
+console.log(sumUp(1, 2, 3)); // 6
+console.log(sumUp(1, 2)); // NaN
+console.log(sumUp(1)); // NaN
