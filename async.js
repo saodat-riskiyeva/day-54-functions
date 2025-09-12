9422bf59124d26642b17fb6a07b2ee83ec1ca0f2
SR <saodat.riskiyeva@gmail.com>
2025-09-12 12:27:17 -0400

    using promises

--- async.js
+++ async.js
@@ -1,8 +1,16 @@
-const fs = require("fs");
+const fs = require("fs/promises");
 
 function readFile() {
   let fileData;
-  fs.readFile("data.txt", function (error, fileData) {
+
+  //   fs.readFile("data.txt", function (error, fileData) {
+  //     console.log("File parsing is done!");
+  //     console.log(fileData.toString());
+
+  //     // start another async task that sends the data to a database
+  //   });
+
+  fs.readFile("data.txt").then(function (fileData) {
     console.log("File parsing is done!");
     console.log(fileData.toString());
   });
