dd25565f0f9655267d4e701c367e1948a2a023bc
SR <saodat.riskiyeva@gmail.com>
2025-09-12 12:14:17 -0400

    asynchronous code and callback function

---
+++ async.js
@@ -0,0 +1,13 @@
+const fs = require("fs");
+
+function readFile() {
+  let fileData;
+  fs.readFile("data.txt", function (error, fileData) {
+    console.log("File parsing is done!");
+    console.log(fileData.toString());
+  });
+
+  console.log("Hi there!");
+}
+
+readFile();
