a39594c73a13a044847f5d2d0d6b2e741248e10f
SR <saodat.riskiyeva@gmail.com>
2025-09-17 16:14:14 -0400

    removing excessive code

--- async.js
+++ async.js
@@ -6,14 +6,10 @@ async function readFile() {
   try {
     fileData = await fs.readFile("data.txt");
   } catch (error) {
-    console.log(error);
     return;
   }
 
-  console.log("File parsing is done!");
   console.log(fileData.toString());
-
-  console.log("Hi there!");
 }
 
 readFile();
